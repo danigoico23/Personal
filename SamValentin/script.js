f9255a1030f2dd5bf7f4b6edf514c5daedb31c1c
danigoico <danielgoicog@alumnos.cei.es>
2025-02-13 22:44:53 +0100

    Sanvalentin cambios

--- SamValentin/script.js
+++ SamValentin/script.js
@@ -12,7 +12,7 @@ function selectOption(option) {
         });
     } else if (option === 'no') {
         // Change text on the "No" button to "You sure?"
-        document.getElementById('no-button').innerText = 'You sure?'; 
+        document.getElementById('no-button').innerText = 'Seguro amorchis?'; 
         // Increase font size of "Yes" button
         var yesButton = document.getElementById('yes-button');
         var currentFontSize = window.getComputedStyle(yesButton).getPropertyValue('font-size');
